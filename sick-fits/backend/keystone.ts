4e18705d1077deedbbfb964d67d8e0c6cc83d8f9
Oleg Konan <o.konan@softteco.com>
2022-02-17 16:49:31 +0300

    lesson 62 done

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -14,6 +14,7 @@ import { CartItem } from './schemas/CartItem';
 import { extendGraphqlSchema } from './mutations';
 import { OrderItem } from './schemas/OrderItem';
 import { Order } from './schemas/Order';
+import { Role } from './schemas/Role';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone';
 
@@ -61,6 +62,7 @@ export default withAuth(
       CartItem,
       OrderItem,
       Order,
+      Role,
     }),
     extendGraphqlSchema,
     ui: {
---
+++ sick-fits/backend/schemas/Role.ts
@@ -0,0 +1,19 @@
+import { list } from '@keystone-next/keystone/schema';
+import { text, relationship } from '@keystone-next/fields';
+import { permissionFields } from './fields';
+
+export const Role = list({
+  fields: {
+    name: text({ isRequired: true }),
+    ...permissionFields,
+    assignedTo: relationship({
+      ref: 'User.role',
+      many: true,
+      ui: {
+        itemView: {
+          fieldMode: 'read',
+        },
+      },
+    }),
+  },
+});
